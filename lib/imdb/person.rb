8e54d85123c62b6df9e41280d8c9463aa524df80
Eric Duminil <eric.duminil@gmail.com>
2018-02-19 14:45:34 +0100

    No inline-rescue anymore for Imdb::Person

--- lib/imdb/person.rb
+++ lib/imdb/person.rb
@@ -8,8 +8,10 @@ module Imdb
       @url = "http://www.imdb.com/name/#{imdb_id}/"
     end
 
+    # NOTE: Can a Person not have a name on IMDB?
     def name
-      document.at("//span[@itemprop='name']").content.strip rescue nil
+      name_html = document.at("//span[@itemprop='name']")
+      name_html.content.strip if name_html
     end
 
     def roles
@@ -17,11 +19,13 @@ module Imdb
     end
 
     def birth_date
-      Date.parse(document.at("//time[@itemprop='birthDate']")['datetime']) rescue nil
+      birth_date_html = document.at("//time[@itemprop='birthDate']")
+      Date.parse(birth_date_html['datetime']) if birth_date_html
     end
 
     def death_date
-      Date.parse(document.at("//time[@itemprop='deathDate']")['datetime']) rescue nil
+      death_date_html = document.at("//time[@itemprop='deathDate']")
+      Date.parse(death_date_html['datetime']) if death_date_html
     end
 
     def age
@@ -33,18 +37,20 @@ module Imdb
       age
     end
 
+    # NOTE: Can a Person not have a bio on IMDB?
     def bio
-      document.at("//div[@itemprop='description']/text()").content.strip rescue nil
+      bio_html = document.at("//div[@itemprop='description']/text()")
+      bio_html.content.strip if bio_html
     end
 
     # Returns an array of Imdb::Movie objects with some data pre-populated
     def known_for
-      document.search("//div[@id='knownfor']/div[contains(@class, 'knownfor-title')]").map do |div|
+      document.search("//div[starts-with(@id, 'knownfor')]/div[contains(@class, 'knownfor-title')]").map do |div|
         begin
-          imdb_id = div.at("div[@class='knownfor-title-role']/a")['href'].match(/title\/tt([0-9]+)/)[1]
+          imdb_id = div.at("div[@class='knownfor-title-role']/a")['href'][/(?<=title\/tt)\d+/]
           movie = Imdb::Movie.new(imdb_id)
           movie.title = div.at("div[@class='knownfor-title-role']/a").content
-          movie.year = div.at("div[@class='knownfor-year']/span").content.match(/\(([0-9\-]+)\)/)[1].to_i
+          movie.year = div.at("div[@class='knownfor-year']/span").content[/\d{4}/].to_i
           movie.poster_thumbnail = div.at('img')['src']
           movie.related_person = self
           movie.related_person_role = div.at("div[@class='knownfor-title-role']/span").content
@@ -56,19 +62,23 @@ module Imdb
     end
 
     def picture_thumbnail
-      document.at("//img[@id='name-poster']")['src']
+      img_html = document.at("//img[@id='name-poster']")
+      img_html['src'] if img_html
     end
 
     def award_highlight
-      document.at("//span[@itemprop='awards']/b").content.gsub(/[[:space:]]+/, ' ').strip rescue nil
+      award_html = document.at("//span[@itemprop='awards']/b")
+      award_html.content.gsub(/[[:space:]]+/, ' ').strip if award_html
     end
 
     def nickname
-      document.at("//div[h4[text()='Nickname:']]/text()[2]").content.strip rescue nil
+      nickname_html = document.at("//div[h4[text()='Nickname:']]/text()[2]")
+      nickname_html.content.strip if nickname_html
     end
 
     def personal_quote
-      document.at("//div[h4[text()='Personal Quote:']]").content.delete("\r\n").strip.gsub(/^Personal Quote:/, '').gsub(/\s\s+See more.*/, '') rescue nil
+      quote_html = document.at("//div[h4[text()='Personal Quote:']]")
+      quote_html.content.delete("\r\n").strip.gsub(/^Personal Quote:/, '').gsub(/\s\s+See more.*/, '') if quote_html
     end
 
     def alternative_names
